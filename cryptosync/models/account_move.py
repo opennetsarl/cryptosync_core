2972c1b62b284120b4b02fccce791c24d96592fe
Yannis Burkhalter <ybu@open-net.ch>
2025-03-26 15:42:12 +0100

    [FIX] cryptosync: Fix tx state on move unlink

--- cryptosync/models/account_move.py
+++ cryptosync/models/account_move.py
@@ -5,3 +5,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     crypto_transaction_id = fields.Many2one("crypto.transaction", string="Crypto Transaction", readonly=True)
+
+    def unlink(self):
+        self.crypto_transaction_id.output_ids.state = "ready"
+        return super().unlink()
