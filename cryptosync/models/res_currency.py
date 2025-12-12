9ad4bd7df737ec405e22c172d1d11ed3bc670482
Yannis Burkhalter <yannis@burkhalter.dev>
2025-12-12 21:51:35 +0100

    [FIX] cryptosync: Only fetch rates for currencies that doesn't have one already for «today» That allows to run the cron hourly (instead of daily) without spamming the provider API After this commit, for a common base, rates should normaly be synced after 1 to 3 hours after UTC+0, depending on the number of set (crypto)currencies

--- cryptosync/models/res_currency.py
+++ cryptosync/models/res_currency.py
@@ -49,7 +49,10 @@ class ResCurrency(models.Model):
                 )
 
     def _cron_crypto_rate(self):
-        currencies = self.search([("crypto_rate_provider_id", "!=", False)])
+        existing_rates = self.env["res.currency.rate"].search([("name", "=", fields.Date.today())])
+        currencies = self.search(
+            [("crypto_rate_provider_id", "!=", False), ("currency_id", "not in", existing_rates.currency_id.ids)]
+        )
         for currency in currencies:
             try:
                 currency.get_crypto_currency_rate()
